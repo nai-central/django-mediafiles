5cd638908d94121895ab161b57df1a7bfdaa1f82
Shige Abe <shige.abe@nasa.gov>
2013-09-27 22:15:15 -0700

    Started app.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mediafiles',
+    version="0.1",
+    author="Shige Abe",
+    packages=find_packages(),
+    include_package_data=True,
+)
